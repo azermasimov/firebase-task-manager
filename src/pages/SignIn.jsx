9f8cdaaa17d7b923d18808e8b58c9a389dbdd5ce
Azer Masimov <mesimovazer@gmail.com>
2022-03-31 11:19:05 +0400

    sign-in sign-up forms

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <>
+      <div>
+        <header>
+          <p>Welcome Back</p>
+        </header>
+
+        <form>
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+
+          <div>
+            <input
+              type={showPassword === false ? "password" : "text"}
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <p onClick={() => setShowPassword((prevState) => !prevState)}>Show Password</p>
+        
+          <Link to="/forgot-password">
+            Forgot Password
+          </Link>
+
+          <div>
+            <button>Sign In</button>
+          </div>
+        </form>
+
+        {/* Google OAuth */}
+
+        <Link to="/sign-up">
+          Sign Up instead
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default SignIn;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
-    <div>SignUp</div>
-  )
+    <>
+      <div>
+        <header>
+          <p>Welcome Back</p>
+        </header>
+
+        <form>
+          <input
+            type="text"
+            placeholder="Name"
+            id="name"
+            value={name}
+            onChange={onChange}
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+
+          <div>
+            <input
+              type={showPassword === false ? "password" : "text"}
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <p onClick={() => setShowPassword((prevState) => !prevState)}>Show Password</p>
+
+          <Link to="/forgot-password">Forgot Password</Link>
+
+          <div>
+            <button>Sign Up</button>
+          </div>
+        </form>
+
+        {/* Google OAuth */}
+
+        <Link to="/sign-in">Sign In instead</Link>
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
