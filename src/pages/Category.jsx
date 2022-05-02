5b9cc7813301e25255eae9c29a96e7cc6b2aa093
Azer Masimov <mesimovazer@gmail.com>
2022-05-02 12:13:56 +0400

    Fetch Products from Firebase

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,87 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+
 function Category() {
-  return <div>Category</div>;
+  const [products, setProducts] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        // Get reference
+        const productsRef = collection(db, "products");
+
+        // Create a query
+        const q = query(
+          productsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        // Execute query
+        const querySnap = await getDocs(q);
+
+        const products = [];
+
+        querySnap.forEach((doc) => {
+          return products.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setProducts(products);
+        setLoading(false);
+      } catch (err) {
+        toast.error("Couldn't fetch products");
+      }
+    };
+
+    fetchProducts();
+  }, [params.categoryName]);
+
+  return (
+    <div>
+      <header>
+        <p>
+          {params.categoryName === "male"
+            ? "Products for male"
+            : "Products for female"}
+        </p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : products && products.length > 0 ? (
+        <>
+          <main>
+            <ul>
+              {products.map((product) => (
+                <h3 key={product.id}>{product.data.productName}</h3>
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No Products for {params.categoryName}</p>
+      )}
+    </div>
+  );
 }
 
 export default Category;
