4c9abebb37ce73e58953bee9a0c64de7e38478ac
Azer Masimov <mesimovazer@gmail.com>
2022-05-04 10:12:26 +0400

    Product Details

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
+import Spinner from "../components/Spinner";
 
 function Product() {
   const [product, setProduct] = useState(null);
@@ -14,7 +15,7 @@ function Product() {
   const auth = getAuth();
 
   useEffect(() => {
-    (async function fetchProduct () {
+    (async function fetchProduct() {
       const docRef = doc(db, "products", params.productId);
       const docSnap = await getDoc(docRef);
 
@@ -24,10 +25,58 @@ function Product() {
         setLoading(false);
       }
     })();
-
   }, [navigate, params.productId]);
 
-  return <div>Product</div>;
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <main>
+      {/* SLIDER */}
+
+      <div
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <button>Copy</button>
+      </div>
+
+      {shareLinkCopied && <p>Link Copied!</p>}
+
+      <div>
+        <p>
+          {product.productName} - $
+          {product.offer
+            ? product.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : product.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </p>
+        <p>
+          ${product.offer && product.regularPrice - product.discountedPrice}{" "}
+          Discount
+        </p>
+        <p>{product.category}</p>
+        <p>For {product.type}</p>
+
+        {auth.currentUser?.uid !== product.userRef && (
+            <Link 
+                to={`/contact/${product.userRef}?productName=${product.productName}`}
+            >
+                Book Now
+            </Link>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default Product;
